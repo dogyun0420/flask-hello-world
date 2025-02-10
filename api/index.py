351e547d79832e92a82886140fb1673ef58cdb66
김도균 <bogyun0420@gmail.com>
2025-02-10 15:42:14 +0900

    test

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+import csv
 
 app = Flask(__name__)
 
+df = list(csv.reader(open('data/2019.csv', 'r')))
+
 @app.route('/')
 def home():
-    return 'Hello, World!###'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/countries')
+def countries():
+    return jsonify(sorted([r[1] for r in df[1:]]))
